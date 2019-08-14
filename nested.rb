c0afa206d29c4529352970a81e752260b0dda746
John Plantamura <plantamurajk@gmail.com>
2019-08-14 13:45:57 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -80,6 +80,10 @@ def adding_matz
         :dennis_ritchie => {
           :known_for => "Unix",
           :languages => ["C"]
+        },
+        :yukihiro_matsumoto => {
+          :known_for => "Ruby",
+          :languages => ["LISP", "C"]
         }
      }
 
